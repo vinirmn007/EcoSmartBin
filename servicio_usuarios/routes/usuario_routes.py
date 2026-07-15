b39f14d0a26761f5819c9e7aee6cffacb1dc3aeb
josue <josutrres@gmail.com>
2026-07-14 22:58:30 -0500

    CI/CD servicio-usuarios

--- servicio_usuarios/routes/usuario_routes.py
+++ servicio_usuarios/routes/usuario_routes.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from supabase import create_client, Client
-from jose import jwt, JWTError
 from schemas.usuario_schemas import UserRegisterSchema, UserLoginSchema, RecoverPasswordSchema, ResetPasswordSchema
 from supabase_auth import UserAttributes
 from models.usuario_model import PerfilUsuario 
